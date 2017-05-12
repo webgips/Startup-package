fc91110f6e4151c74623420b5a52282ffdee6695
Maxim Markov <webgips@gmail.com>
2017-05-12 13:44:43 +0300

    init

---
+++ gulp/paths/css.foundation.js
@@ -0,0 +1,7 @@
+'use strict';
+// Пути к  внешним плагинам и библиотекам css
+module.exports = [
+  './node_modules/normalize.css/normalize.css',
+  './node_modules/slick-carousel/slick/slick.css',
+  './node_modules/fancybox/dist/css/jquery.fancybox.css'
+];
---
+++ gulp/paths/js.foundation.js
@@ -0,0 +1,7 @@
+'use strict';
+// Пути к внешним плагинам и библиотекам javascript
+module.exports = [
+  './node_modules/jquery/dist/jquery.min.js',
+  './node_modules/slick-carousel/slick/slick.min.js',
+  './node_modules/fancybox/dist/js/jquery.fancybox.js'  
+];
